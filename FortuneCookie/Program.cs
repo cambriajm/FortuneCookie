dee072d2007b99470a93d51e6319742865e04c11
cambriajm <cambriajmorgan@gmail.com>
2026-02-05 08:48:46 -0700

    need to add actual fortunes and not just dumb randomness. working on a yes/no for new fortune

--- FortuneCookie/Program.cs
+++ FortuneCookie/Program.cs
@@ -4,7 +4,67 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a fortune cookie." +
+                "\nUnfortunately, theres no actual cookies involved." +
+                "\nI like snickerdoodle cookies." +
+                "\nAnyways, heres your fortune." +
+                "\n" +
+                "\n");
+            Random rand = new Random(); // constructor
+
+            //test randomness and range with a for loop
+            for (int i = 0; i < 1; i++)
+            {
+                //rand.Next(min, max) max is not inclusive 
+                int n = rand.Next(1, 11); //random number between one and six
+
+                while(true)
+                {
+
+                switch (n)
+                {
+                    case 0:
+                        Console.WriteLine("You might want to check your engine oil :)");
+                        break; 
+
+                    case 1:
+                        Console.WriteLine("yeehaw");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("no");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("AAAAA");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("b");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("bb");
+                        break;
+
+                    case 6:
+                        Console.WriteLine("c");
+                        break;
+
+                    case 7:
+                        Console.WriteLine("d");
+                        break;
+
+                    case 8:
+                        Console.WriteLine("yedehaw");
+                        break;
+                }
+
+                }
+            }
+            //pause
+            Console.Read();
         }
+
     }
 }

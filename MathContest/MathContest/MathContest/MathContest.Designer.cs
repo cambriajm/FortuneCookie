a57b45ec97f196c2934473fc6f18e94227461d16
cambriajm <cambriajm@CAMDELL>
2026-04-28 11:40:23 -0600

    yeehaw

--- MathContest/MathContest/MathContest/MathContest.Designer.cs
+++ MathContest/MathContest/MathContest/MathContest.Designer.cs
@@ -38,55 +38,75 @@
             AgeLabel = new Label();
             GradeTextBox = new TextBox();
             AgeTextBox = new TextBox();
+            StudentInfoGroupBox = new GroupBox();
+            AddButton = new RadioButton();
+            SubtractButton = new RadioButton();
+            DivideButton = new RadioButton();
+            MultiplyButton = new RadioButton();
+            ButtonGroupBox = new GroupBox();
+            FirstNumberTextBox = new TextBox();
+            SecondNumberTextBox = new TextBox();
+            StudentAnswerTextBox = new TextBox();
+            FirstNumberLabel = new Label();
+            SecondNumberLabel = new Label();
+            StudentAnswerLabel = new Label();
+            MathGroupBox = new GroupBox();
+            StudentInfoGroupBox.SuspendLayout();
+            ButtonGroupBox.SuspendLayout();
+            MathGroupBox.SuspendLayout();
             SuspendLayout();
             // 
             // SubmitButton
             // 
             SubmitButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            SubmitButton.Location = new Point(734, 12);
+            SubmitButton.Location = new Point(741, 102);
             SubmitButton.Margin = new Padding(4, 3, 4, 3);
             SubmitButton.Name = "SubmitButton";
             SubmitButton.Size = new Size(156, 65);
             SubmitButton.TabIndex = 0;
             SubmitButton.Text = "Submit";
             SubmitButton.UseVisualStyleBackColor = true;
+            SubmitButton.Click += SubmitButton_Click;
             // 
             // ClearButton
             // 
             ClearButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            ClearButton.Location = new Point(734, 97);
+            ClearButton.Location = new Point(741, 187);
             ClearButton.Margin = new Padding(4, 3, 4, 3);
             ClearButton.Name = "ClearButton";
             ClearButton.Size = new Size(156, 65);
             ClearButton.TabIndex = 1;
-            ClearButton.Text = "Button";
+            ClearButton.Text = "Clear";
             ClearButton.UseVisualStyleBackColor = true;
+            ClearButton.Click += ClearButton_Click;
             // 
             // ExitButton
             // 
             ExitButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            ExitButton.Location = new Point(734, 267);
+            ExitButton.Location = new Point(741, 357);
             ExitButton.Margin = new Padding(4, 3, 4, 3);
             ExitButton.Name = "ExitButton";
             ExitButton.Size = new Size(156, 65);
             ExitButton.TabIndex = 3;
             ExitButton.Text = "Exit";
             ExitButton.UseVisualStyleBackColor = true;
+            ExitButton.Click += ExitButton_Click;
             // 
             // SummaryButton
             // 
             SummaryButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            SummaryButton.Location = new Point(734, 182);
+            SummaryButton.Location = new Point(741, 272);
             SummaryButton.Margin = new Padding(4, 3, 4, 3);
             SummaryButton.Name = "SummaryButton";
             SummaryButton.Size = new Size(156, 65);
             SummaryButton.TabIndex = 2;
             SummaryButton.Text = "Summary";
             SummaryButton.UseVisualStyleBackColor = true;
+            SummaryButton.Click += SummaryButton_Click;
             // 
             // StudentNameTextBox
             // 
-            StudentNameTextBox.Location = new Point(165, 46);
+            StudentNameTextBox.Location = new Point(163, 34);
             StudentNameTextBox.Margin = new Padding(4, 3, 4, 3);
             StudentNameTextBox.Name = "StudentNameTextBox";
             StudentNameTextBox.Size = new Size(194, 31);
@@ -95,7 +115,7 @@
             // StudentNameLabel
             // 
             StudentNameLabel.AutoSize = true;
-            StudentNameLabel.Location = new Point(18, 54);
+            StudentNameLabel.Location = new Point(16, 42);
             StudentNameLabel.Margin = new Padding(4, 0, 4, 0);
             StudentNameLabel.Name = "StudentNameLabel";
             StudentNameLabel.Size = new Size(139, 23);
@@ -105,7 +125,7 @@
             // GradeLabel
             // 
             GradeLabel.AutoSize = true;
-            GradeLabel.Location = new Point(522, 54);
+            GradeLabel.Location = new Point(520, 42);
             GradeLabel.Margin = new Padding(4, 0, 4, 0);
             GradeLabel.Name = "GradeLabel";
             GradeLabel.Size = new Size(66, 23);
@@ -115,7 +135,7 @@
             // AgeLabel
             // 
             AgeLabel.AutoSize = true;
-            AgeLabel.Location = new Point(367, 54);
+            AgeLabel.Location = new Point(365, 42);
             AgeLabel.Margin = new Padding(4, 0, 4, 0);
             AgeLabel.Name = "AgeLabel";
             AgeLabel.Size = new Size(44, 23);
@@ -125,7 +145,7 @@
             // 
             // GradeTextBox
             // 
-            GradeTextBox.Location = new Point(419, 46);
+            GradeTextBox.Location = new Point(417, 34);
             GradeTextBox.Margin = new Padding(4, 3, 4, 3);
             GradeTextBox.Name = "GradeTextBox";
             GradeTextBox.Size = new Size(73, 31);
@@ -133,23 +153,155 @@
             // 
             // AgeTextBox
             // 
-            AgeTextBox.Location = new Point(596, 46);
+            AgeTextBox.Location = new Point(594, 34);
             AgeTextBox.Margin = new Padding(4, 3, 4, 3);
             AgeTextBox.Name = "AgeTextBox";
             AgeTextBox.Size = new Size(73, 31);
             AgeTextBox.TabIndex = 9;
+            // 
+            // StudentInfoGroupBox
+            // 
+            StudentInfoGroupBox.Controls.Add(AgeTextBox);
+            StudentInfoGroupBox.Controls.Add(GradeTextBox);
+            StudentInfoGroupBox.Controls.Add(AgeLabel);
+            StudentInfoGroupBox.Controls.Add(GradeLabel);
+            StudentInfoGroupBox.Controls.Add(StudentNameLabel);
+            StudentInfoGroupBox.Controls.Add(StudentNameTextBox);
+            StudentInfoGroupBox.Location = new Point(11, 12);
+            StudentInfoGroupBox.Name = "StudentInfoGroupBox";
+            StudentInfoGroupBox.Size = new Size(684, 102);
+            StudentInfoGroupBox.TabIndex = 10;
+            StudentInfoGroupBox.TabStop = false;
+            StudentInfoGroupBox.Text = "Student Information";
+            // 
+            // AddButton
+            // 
+            AddButton.AutoSize = true;
+            AddButton.Location = new Point(78, 49);
+            AddButton.Name = "AddButton";
+            AddButton.Size = new Size(68, 27);
+            AddButton.TabIndex = 11;
+            AddButton.TabStop = true;
+            AddButton.Text = "Add";
+            AddButton.UseVisualStyleBackColor = true;
+            // 
+            // SubtractButton
+            // 
+            SubtractButton.AutoSize = true;
+            SubtractButton.Location = new Point(78, 95);
+            SubtractButton.Name = "SubtractButton";
+            SubtractButton.Size = new Size(108, 27);
+            SubtractButton.TabIndex = 12;
+            SubtractButton.TabStop = true;
+            SubtractButton.Text = "Subtract";
+            SubtractButton.UseVisualStyleBackColor = true;
+            // 
+            // DivideButton
+            // 
+            DivideButton.AutoSize = true;
+            DivideButton.Location = new Point(78, 187);
+            DivideButton.Name = "DivideButton";
+            DivideButton.Size = new Size(90, 27);
+            DivideButton.TabIndex = 14;
+            DivideButton.TabStop = true;
+            DivideButton.Text = "Divide";
+            DivideButton.UseVisualStyleBackColor = true;
+            // 
+            // MultiplyButton
+            // 
+            MultiplyButton.AutoSize = true;
+            MultiplyButton.Location = new Point(78, 141);
+            MultiplyButton.Name = "MultiplyButton";
+            MultiplyButton.Size = new Size(107, 27);
+            MultiplyButton.TabIndex = 13;
+            MultiplyButton.TabStop = true;
+            MultiplyButton.Text = "Multiply";
+            MultiplyButton.UseVisualStyleBackColor = true;
+            // 
+            // ButtonGroupBox
+            // 
+            ButtonGroupBox.Controls.Add(DivideButton);
+            ButtonGroupBox.Controls.Add(MultiplyButton);
+            ButtonGroupBox.Controls.Add(SubtractButton);
+            ButtonGroupBox.Controls.Add(AddButton);
+            ButtonGroupBox.Location = new Point(449, 182);
+            ButtonGroupBox.Name = "ButtonGroupBox";
+            ButtonGroupBox.Size = new Size(246, 258);
+            ButtonGroupBox.TabIndex = 15;
+            ButtonGroupBox.TabStop = false;
+            ButtonGroupBox.Text = "Equation Type";
+            // 
+            // FirstNumberTextBox
+            // 
+            FirstNumberTextBox.Location = new Point(173, 45);
+            FirstNumberTextBox.Name = "FirstNumberTextBox";
+            FirstNumberTextBox.Size = new Size(194, 31);
+            FirstNumberTextBox.TabIndex = 16;
+            // 
+            // SecondNumberTextBox
+            // 
+            SecondNumberTextBox.Location = new Point(173, 115);
+            SecondNumberTextBox.Name = "SecondNumberTextBox";
+            SecondNumberTextBox.Size = new Size(194, 31);
+            SecondNumberTextBox.TabIndex = 17;
+            // 
+            // StudentAnswerTextBox
+            // 
+            StudentAnswerTextBox.Location = new Point(173, 182);
+            StudentAnswerTextBox.Name = "StudentAnswerTextBox";
+            StudentAnswerTextBox.Size = new Size(194, 31);
+            StudentAnswerTextBox.TabIndex = 18;
+            // 
+            // FirstNumberLabel
+            // 
+            FirstNumberLabel.AutoSize = true;
+            FirstNumberLabel.Location = new Point(26, 49);
+            FirstNumberLabel.Name = "FirstNumberLabel";
+            FirstNumberLabel.Size = new Size(132, 23);
+            FirstNumberLabel.TabIndex = 19;
+            FirstNumberLabel.Text = "First Number";
+            // 
+            // SecondNumberLabel
+            // 
+            SecondNumberLabel.AutoSize = true;
+            SecondNumberLabel.Location = new Point(10, 118);
+            SecondNumberLabel.Name = "SecondNumberLabel";
+            SecondNumberLabel.Size = new Size(155, 23);
+            SecondNumberLabel.TabIndex = 20;
+            SecondNumberLabel.Text = "Second Number";
+            // 
+            // StudentAnswerLabel
+            // 
+            StudentAnswerLabel.AutoSize = true;
+            StudentAnswerLabel.Location = new Point(12, 185);
+            StudentAnswerLabel.Name = "StudentAnswerLabel";
+            StudentAnswerLabel.Size = new Size(155, 23);
+            StudentAnswerLabel.TabIndex = 21;
+            StudentAnswerLabel.Text = "Student Answer";
+            // 
+            // MathGroupBox
+            // 
+            MathGroupBox.Controls.Add(StudentAnswerLabel);
+            MathGroupBox.Controls.Add(SecondNumberLabel);
+            MathGroupBox.Controls.Add(FirstNumberLabel);
+            MathGroupBox.Controls.Add(StudentAnswerTextBox);
+            MathGroupBox.Controls.Add(SecondNumberTextBox);
+            MathGroupBox.Controls.Add(FirstNumberTextBox);
+            MathGroupBox.Location = new Point(23, 182);
+            MathGroupBox.Name = "MathGroupBox";
+            MathGroupBox.Size = new Size(404, 258);
+            MathGroupBox.TabIndex = 22;
+            MathGroupBox.TabStop = false;
+            MathGroupBox.Text = "Current Math Problem";
             // 
             // MathContest
             // 
             AutoScaleDimensions = new SizeF(12F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(930, 505);
-            Controls.Add(AgeTextBox);
-            Controls.Add(GradeTextBox);
-            Controls.Add(AgeLabel);
-            Controls.Add(GradeLabel);
-            Controls.Add(StudentNameLabel);
-            Controls.Add(StudentNameTextBox);
+            Controls.Add(MathGroupBox);
+            Controls.Add(ButtonGroupBox);
+            Controls.Add(StudentInfoGroupBox);
             Controls.Add(ExitButton);
             Controls.Add(SummaryButton);
             Controls.Add(ClearButton);
@@ -158,8 +310,13 @@
             Margin = new Padding(4, 3, 4, 3);
             Name = "MathContest";
             Text = "Math Contest";
+            StudentInfoGroupBox.ResumeLayout(false);
+            StudentInfoGroupBox.PerformLayout();
+            ButtonGroupBox.ResumeLayout(false);
+            ButtonGroupBox.PerformLayout();
+            MathGroupBox.ResumeLayout(false);
+            MathGroupBox.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -174,5 +331,18 @@
         private Label AgeLabel;
         private TextBox GradeTextBox;
         private TextBox AgeTextBox;
+        private GroupBox StudentInfoGroupBox;
+        private RadioButton AddButton;
+        private RadioButton SubtractButton;
+        private RadioButton DivideButton;
+        private RadioButton MultiplyButton;
+        private GroupBox ButtonGroupBox;
+        private TextBox FirstNumberTextBox;
+        private TextBox SecondNumberTextBox;
+        private TextBox StudentAnswerTextBox;
+        private Label FirstNumberLabel;
+        private Label SecondNumberLabel;
+        private Label StudentAnswerLabel;
+        private GroupBox MathGroupBox;
     }
 }
--- MathContest/MathContest/MathContest/MathContest.cs
+++ MathContest/MathContest/MathContest/MathContest.cs
@@ -1,12 +1,257 @@
 namespace MathContest
+//Cambria Morgan
+//Spring 2026
+//RCET 2265
+//Math Contest
+//https://github.com/cambriajm/MathContest.git
 {
     public partial class MathContest : Form
     {
         public MathContest()
         {
             InitializeComponent();
+            SetDefaults();
+        }
+        int submitNumber = 0;
+        int correctNumber = 0;
+        int correct = 0;
+
+
+        private void FirstNumberGenerator()
+        {
+            Random random = new Random();
+            int firstNumber = random.Next(1, 11);
+            FirstNumberTextBox.Text = firstNumber.ToString();
+        }
+        private void SecondNumberGenerator()
+        {
+            Random random = new Random();
+            int secondNumber = random.Next(1, 11);
+            SecondNumberTextBox.Text = secondNumber.ToString();
+        }
+        void MathFunction()
+        {
+            int firstNumber = int.Parse(FirstNumberTextBox.Text);
+            int secondNumber = int.Parse(SecondNumberTextBox.Text);
+
+            if (AddButton.Checked)
+            {
+                correct = firstNumber + secondNumber;
+            }
+            else if (SubtractButton.Checked)
+            {
+                correct = firstNumber - secondNumber;
+            }
+            else if (DivideButton.Checked)
+            {
+                correct = firstNumber / secondNumber;
+            }
+            else if (MultiplyButton.Checked)
+            {
+                correct = firstNumber * secondNumber;
+            }
         }
 
+        private void SetDefaults()
+        {
+            AgeTextBox.Text = "";
+            StudentNameTextBox.Text = "";
+            GradeTextBox.Text = "";
+            AgeTextBox.BackColor = Color.LightBlue;
+            StudentNameTextBox.BackColor = Color.LightBlue;
+            GradeTextBox.BackColor = Color.LightBlue;
+            FirstNumberTextBox.Text = "";
+            SecondNumberTextBox.Text = "";
+            StudentAnswerTextBox.Text = "";
+            FirstNumberTextBox.Enabled = false;
+            SecondNumberTextBox.Enabled = false;
+            StudentAnswerTextBox.Enabled = false;
+            SummaryButton.Enabled = false;
+            SubmitButton.Enabled = false;
+            AddButton.Checked = true;
+            submitNumber = 0;
+            correct = 0;
+            correctNumber = 0;
+        }
+
+        private bool ValidateFields()
+        {
+            bool valid = true;
+
+            if (StudentNameTextBox.Text! == "")
+            {
+                StudentNameTextBox.BackColor = Color.White;
+            }
+            else
+            {
+                StudentNameTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+
+            try
+            {
+               
+                
+                if (int.Parse(AgeTextBox.Text) >= 7 && int.Parse(AgeTextBox.Text) <= 11)
+                {
+                    AgeTextBox.BackColor = Color.White;
+                }
+                else
+
+                {
+                    AgeTextBox.BackColor = Color.LightYellow;
+                    valid = false;
+                }
+            }
+            catch (Exception)
+            {
+                AgeTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+
+            try
+            {
+                if (int.Parse(GradeTextBox.Text) >= 1 && int.Parse(GradeTextBox.Text) <= 4)
+                {
+                    GradeTextBox.BackColor = Color.White;
+                }
+                else
+                {
+                    GradeTextBox.BackColor = Color.LightYellow;
+                    valid = false;
+                }
+            }
+            catch (Exception)
+            {
+                GradeTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+
+            if (StudentAnswerTextBox.Text != "")
+            {
+                StudentAnswerTextBox.BackColor = Color.White;
+            }
+            else
+            {
+                StudentAnswerTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+
+            return valid;
+        }
         
+
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            SetDefaults();
+        }
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            MathFunction();
+            try
+            {
+                int.Parse(StudentAnswerTextBox.Text);
+            }
+            catch (Exception)
+            {
+                int.Parse(AgeTextBox.Text);
+            }
+            if (int.Parse(StudentAnswerTextBox.Text) == correct)
+            {
+                MessageBox.Show("Correct answer!");
+                correctNumber++;
+            }
+            else
+            {
+                MessageBox.Show($"Whops,{correct} is correct.");
+            }
+
+            FirstNumberGenerator();
+            SecondNumberGenerator();
+            SummaryButton.Enabled = true;
+            submitNumber++;
+        }
+
+        private void SummaryButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"You have {correct} out of {submitNumber}");
+        }
+        private void StudentNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (submitNumber >= 1)
+            {
+                SetDefaults();
+            }
+            else
+            {
+                if (ValidateFields())
+                {
+                    SubmitButton.Enabled = true;
+                }
+                else
+                {
+                    SubmitButton.Enabled = false;
+                }
+            }
+        }
+
+        private void AgeTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (submitNumber >= 1)
+
+            {
+                SetDefaults();
+            }
+            else
+            {
+
+                if (ValidateFields())
+                {
+                    SubmitButton.Enabled = true;
+                }
+                else
+                {
+                    SubmitButton.Enabled = false;
+                }
+            }
+        }
+
+        private void GradeTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (submitNumber >= 1)
+            {
+                SetDefaults();
+            }
+            else
+            {
+                if (ValidateFields())
+                {
+                    SubmitButton.Enabled = true;
+                }
+                else
+                {
+                    SubmitButton.Enabled = false;
+                }
+            }
+        }
+
+        private void StudentAnswerTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (ValidateFields())
+            {
+                SubmitButton.Enabled = true;
+            }
+            else
+            {
+                SubmitButton.Enabled = false;
+            }
+        }
     }
 }
+

9015b295280af6d6e832dc7ed2c3aa65e3f64a07
cambriajm <cambriajm@CAMDELL>
2026-05-02 15:36:42 -0600

    AAAAAAAAAAAA

--- StansGrocery/StansGrocery/StansGrocery/StansGrocery.cs
+++ StansGrocery/StansGrocery/StansGrocery/StansGrocery.cs
@@ -5,8 +5,59 @@ namespace StansGrocery
         public StansGrocery()
         {
             InitializeComponent();
+
+            SplashScreenForm();
+            FileToArray(filePath);
+            DisplayData();
         }
 
       
-    }
+    
+      private void SplashScreenForm()//this does the time for the screen to load then go away 
+        {
+            SplashScreenForm splashForm = new SplashScreenForm();
+            splashForm.Show();
+            System.Threading.Thread.Sleep(3000);
+            splashForm.Close();
+        }
+
+
+        int CountOfLinesIn(string filePath)
+        {
+            int count = 0;
+            using (StreamReader testFile = new StreamReader(filePath))
+            {
+                do
+                {
+                    testFile.ReadLine();
+                    count++;
+                } while (!testFile.EndOfStream);
+            }
+            return count;
+        }
+        void FileToArray(string filePath)
+        {
+            string[,] _customerData = new string[4, CountOfLinesIn(filePath)];
+            string[] temp;
+            int counter = 0;
+
+            using (StreamReader testFile = new StreamReader(filePath))
+            {
+                do
+                {
+                    temp = testFile.ReadLine().Split(",");
+                    if (temp.Length >=3)
+                    {
+                        for (int i = 0; i < temp.Length && i < 4; i++)
+                        {
+                            _customerData[i, counter] = CleanString(temp[i]);
+                        }
+                    }
+                    counter++;
+                } while (!testFile.EndOfStream);
+            }
+            this.customerData = _customerData;
+        }
+
+    } 
 }
